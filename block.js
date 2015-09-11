1275cff6ff51b02c4ffa619b0fb73b45bb0cd64c
Wayne Chang <wayne@cmoney.net.tw>
2015-09-11 11:45:14 +0800

    remove console.log

--- block.js
+++ block.js
@@ -1,7 +1,5 @@
 function injectScript(file, node) {
-	console.log(document.getElementsByTagName(node));
-
-    var th = document.getElementsByTagName(node)[0];
+	var th = document.getElementsByTagName(node)[0];
     var s = document.createElement('script');
     s.setAttribute('type', 'text/javascript');
     s.setAttribute('src', file);
