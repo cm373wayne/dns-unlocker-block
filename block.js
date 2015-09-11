d0e5ee8f3d9439ac8244642acff7f7e226b62bf3
Wayne Chang <wayne@cmoney.net.tw>
2015-09-11 11:41:16 +0800

    set variable before <heaa />

--- block.js
+++ block.js
@@ -1,8 +1,10 @@
 function injectScript(file, node) {
+	console.log(document.getElementsByTagName(node));
+
     var th = document.getElementsByTagName(node)[0];
     var s = document.createElement('script');
     s.setAttribute('type', 'text/javascript');
     s.setAttribute('src', file);
     th.appendChild(s);
 }
-injectScript( chrome.extension.getURL('/content.js'), 'body');
+injectScript( chrome.extension.getURL('/content.js'), 'html');
